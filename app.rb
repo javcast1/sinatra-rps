529d0f37f7b7c2a36d569bbb318de16ccd993099
javcast1 <136658443+javcast1@users.noreply.github.com>
2023-07-12 07:59:23 +0000

    plays game

--- app.rb
+++ app.rb
@@ -1,8 +1,44 @@
 require 'sinatra'
+require "sinatra/reloader"
 
-get('/') do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/") do
+  <<~HTML
+    <h1>Welcome to Rock, Paper, Scissors</h1>
+    <p>Click one of the options below to play:</p>
+    <ul>
+      <li><a href="/play/rock">Rock</a></li>
+      <li><a href="/play/paper">Paper</a></li>
+      <li><a href="/play/scissors">Scissors</a></li>
+    </ul>
+  HTML
 end
+
+get("/play/:choice") do
+  choice = params["choice"]
+
+  # Generate a random computer choice
+  computer_choice = ["rock", "paper", "scissors"].sample
+
+  # Determine the game result
+  result = play_game(choice, computer_choice)
+
+  <<~HTML
+    <h1>You chose #{choice.capitalize}</h1>
+    <p>The computer chose #{computer_choice.capitalize}</p>
+    <h2>Result: #{result}</h2>
+    <p><a href="/">Play Again</a></p>
+  HTML
+end
+
+def play_game(player_choice, computer_choice)
+  if player_choice == computer_choice
+    "It's a draw!"
+  elsif (player_choice == "rock" && computer_choice == "scissors") ||
+        (player_choice == "paper" && computer_choice == "rock") ||
+        (player_choice == "scissors" && computer_choice == "paper")
+    "You win!"
+  else
+    "You lose!"
+  end
+end
+
